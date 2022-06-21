41646bc2e66ac8dbba37be318bcc4df541be9e4a
xiphl <xipherl9@gmail.com>
2022-06-21 14:37:34 +0800

    .

--- index.js
+++ index.js
@@ -7,8 +7,9 @@ const OUTPUT_ARTIFACT_NAME = 'artifact_name';
 
 async function runAction(getInput, writeOutput) {
     const imageName = getInput(INPUT_IMAGE, true);
-    console.log(`upload package: before await upload: ${imageName.split(' ')[0]}`);
-    const artifactName = await upload(imageName.split(' ')[0]);
+    console.log(`the length of imageName is ${length(imageName)}`);
+    console.log(`upload package: before await upload: ${imageName.split(/\s+/)[0]}`);
+    const artifactName = await upload(imageName.split(/\s+/)[0]);
     console.log(`upload package: after await upload`);
     writeOutput(OUTPUT_ARTIFACT_NAME, artifactName);
 }
